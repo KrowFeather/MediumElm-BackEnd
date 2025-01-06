ebd9c8aa928daf1782a76d713a38da9d3e706b74
KrowFeather <2625056626@qq.com>
2025-01-07 06:57:44 +0800

    v1.2.2

--- src/main/java/com/krowfeather/mediumelm/user/controller/UserController.java
+++ src/main/java/com/krowfeather/mediumelm/user/controller/UserController.java
@@ -1,18 +1,16 @@
 package com.krowfeather.mediumelm.user.controller;
 
 import com.krowfeather.mediumelm.common.Result;
+import com.krowfeather.mediumelm.user.entity.dto.User;
 import com.krowfeather.mediumelm.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
 @Tag(name = "用户管理接口", description = "用于获取和管理用户信息的接口")
 public class UserController {
     private final UserService service;
@@ -35,8 +33,17 @@ public class UserController {
             @ApiResponse(responseCode = "200", description = "成功获取用户信息"),
             @ApiResponse(responseCode = "404", description = "未找到该用户")
     })
-    @GetMapping("/{username}")
+    @GetMapping("/api/users/{username}")
     public Result getUserByUsername(@PathVariable String username) {
         return Result.success(service.findByUsername(username));
+    }
+
+    @PostMapping("/auth/register")
+    public Result register(@RequestBody User user) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        service.save(user);
+        User user1 = service.findByUsername(user.getUsername());
+        return Result.success(user1);
     }
 }
